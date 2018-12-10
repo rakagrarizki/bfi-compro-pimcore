a1b14fee144436717b2a09908f3d0388b673a89e
rifandy <maleakhirifandy@gmail>
2018-12-10 19:02:10 +0700

    navigation update

--- docroot/app/Resources/views/Includes/language.html.php
+++ docroot/app/Resources/views/Includes/language.html.php
@@ -18,3 +18,29 @@ $languageSwitcher = $this->languageSwitcher();
     <?php } ?>
 </div>
 
+
+
+<?php
+// this is an auto-generated language switcher, of course you can create your own
+/*$service = new \Pimcore\Model\Document\Service;
+$translations = $service->getTranslations($this->document);
+$links = [];
+foreach (\Pimcore\Tool::getValidLanguages() as $language) {
+    $target = "/" . $language;
+    if (isset($translations[$language])) {
+        $localizedDocument = \Pimcore\Model\Document::getById($translations[$language]);
+        if ($localizedDocument) {
+            $target = $localizedDocument->getFullPath();
+        }
+    }
+    $links[$language] = $target;
+}*/
+?>
+
+<!--<div class="lang">
+    <?php /*foreach ($links as $lang => $target) {
+        */?>
+        <a href="<?php /*echo $target */?>"><?php /*echo $lang == 'en' ? 'EN' : 'ID' */?></a>
+    <?php /*} */?>
+</div>-->
+
--- docroot/app/Resources/views/Includes/mobile-navigation.html.php
+++ docroot/app/Resources/views/Includes/mobile-navigation.html.php
@@ -10,32 +10,7 @@ use Pimcore\Model\Document;
                 <a href="#" class="cta-top-nav"><?= $this->translate("corporate") ?></a>
             </div>
             <div class="col-xs-6 text-right">
-                <?php
-                // this is an auto-generated language switcher, of course you can create your own
-                $service = new \Pimcore\Model\Document\Service;
-                $translations = $service->getTranslations($this->document);
-                $links = [];
-                foreach (\Pimcore\Tool::getValidLanguages() as $language) {
-                    $target = "/" . $language;
-                    if (isset($translations[$language])) {
-                        $localizedDocument = \Pimcore\Model\Document::getById($translations[$language]);
-                        if ($localizedDocument) {
-                            $target = $localizedDocument->getFullPath();
-                        }
-                    }
-                    $links[$language] = $target;
-                }
-                ?>
-
-                <?php foreach ($links as $lang => $target) {
-                    if($this->getLocale() === $lang){
-                        $active = "active";
-                    }else{
-                        $active = "";
-                    }
-                    ?>
-                    <a href="<?php echo $target ?>" class="cta-top-nav <?= $active ?>"><?php echo $lang == 'en' ? 'EN' : 'ID' ?></a>
-                <?php } ?>
+                <?php echo $this->template("Includes/mobile-language.html.php") ?>
             </div>
         </div>
         <div class="navbar-header">
