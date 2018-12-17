4ee8eb65d2f98d8e83d29756bffec6b37d2d473d
rifandy <maleakhirifandy@gmail>
2018-12-17 18:39:44 +0700

    remove commit

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euv
 cd /home/var/www/php/bfi/docroot
-git commit -am "auto commit by gitlab ci for $CI_COMMIT_SHA"
 git checkout backend-dev
 git fetch
 git checkout $CI_COMMIT_SHA
