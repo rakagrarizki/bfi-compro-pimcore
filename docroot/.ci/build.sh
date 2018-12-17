1d8a15fd6cd49530749f46c8748aaa41e34c2b45
rifandy <maleakhirifandy@gmail>
2018-12-17 11:03:54 +0700

    add continues integration

---
+++ docroot/.ci/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euv
+cd /home/var/www/php/bfi/docroot
+git commit -am "auto commit by gitlab ci for $CI_COMMIT_SHA"
+git checkout development
+git fetch
+git checkout $CI_COMMIT_SHA
+composer install  --no-interaction --optimize-autoloader
