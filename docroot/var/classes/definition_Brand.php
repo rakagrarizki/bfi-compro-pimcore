77442e85095b403cabc9533556cbe6b059b5ab87
rifandy <maleakhirifandy@gmail>
2018-12-21 14:48:27 +0700

    add service for brand

--- docroot/var/classes/definition_Brand.php
+++ docroot/var/classes/definition_Brand.php
@@ -1,42 +1,151 @@
 <?php 
 
 /** 
-* Generated at: 2018-12-21T05:29:42+01:00
+* Generated at: 2018-12-21T08:29:33+01:00
 * Inheritance: no
 * Variants: no
+* Changed by: admin (2)
 * IP: 127.0.0.1
 
 
 Fields Summary: 
+- Name [input]
+- Tipe [select]
 */ 
 
 
 return Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
    'id' => '7',
    'name' => 'Brand',
-   'description' => NULL,
-   'creationDate' => NULL,
-   'modificationDate' => 1545366580,
+   'description' => '',
+   'creationDate' => 0,
+   'modificationDate' => 1545377371,
    'userOwner' => 2,
-   'userModification' => NULL,
-   'parentClass' => NULL,
+   'userModification' => 2,
+   'parentClass' => '',
    'listingParentClass' => '',
    'useTraits' => '',
    'listingUseTraits' => '',
    'allowInherit' => false,
-   'allowVariants' => false,
+   'allowVariants' => NULL,
    'showVariants' => false,
-   'layoutDefinitions' => NULL,
-   'icon' => NULL,
-   'previewUrl' => NULL,
-   'group' => NULL,
+   'layoutDefinitions' => 
+  Pimcore\Model\DataObject\ClassDefinition\Layout\Panel::__set_state(array(
+     'fieldtype' => 'panel',
+     'labelWidth' => 100,
+     'layout' => NULL,
+     'name' => 'pimcore_root',
+     'type' => NULL,
+     'region' => NULL,
+     'title' => NULL,
+     'width' => NULL,
+     'height' => NULL,
+     'collapsible' => false,
+     'collapsed' => false,
+     'bodyStyle' => NULL,
+     'datatype' => 'layout',
+     'permissions' => NULL,
+     'childs' => 
+    array (
+      0 => 
+      Pimcore\Model\DataObject\ClassDefinition\Layout\Panel::__set_state(array(
+         'fieldtype' => 'panel',
+         'labelWidth' => 100,
+         'layout' => NULL,
+         'name' => 'Layout',
+         'type' => NULL,
+         'region' => NULL,
+         'title' => '',
+         'width' => NULL,
+         'height' => NULL,
+         'collapsible' => false,
+         'collapsed' => false,
+         'bodyStyle' => '',
+         'datatype' => 'layout',
+         'permissions' => NULL,
+         'childs' => 
+        array (
+          0 => 
+          Pimcore\Model\DataObject\ClassDefinition\Data\Input::__set_state(array(
+             'fieldtype' => 'input',
+             'width' => NULL,
+             'queryColumnType' => 'varchar',
+             'columnType' => 'varchar',
+             'columnLength' => 190,
+             'phpdocType' => 'string',
+             'regex' => '',
+             'unique' => false,
+             'name' => 'Name',
+             'title' => 'Name',
+             'tooltip' => '',
+             'mandatory' => false,
+             'noteditable' => false,
+             'index' => false,
+             'locked' => NULL,
+             'style' => '',
+             'permissions' => NULL,
+             'datatype' => 'data',
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => false,
+             'visibleSearch' => false,
+          )),
+          1 => 
+          Pimcore\Model\DataObject\ClassDefinition\Data\Select::__set_state(array(
+             'fieldtype' => 'select',
+             'options' => 
+            array (
+              0 => 
+              array (
+                'key' => 'MOTOR',
+                'value' => '2',
+              ),
+              1 => 
+              array (
+                'key' => 'MOBIL',
+                'value' => '1',
+              ),
+            ),
+             'width' => '',
+             'defaultValue' => '',
+             'optionsProviderClass' => '',
+             'optionsProviderData' => '',
+             'queryColumnType' => 'varchar',
+             'columnType' => 'varchar',
+             'columnLength' => 190,
+             'phpdocType' => 'string',
+             'name' => 'Tipe',
+             'title' => 'Tipe',
+             'tooltip' => '',
+             'mandatory' => false,
+             'noteditable' => false,
+             'index' => false,
+             'locked' => NULL,
+             'style' => '',
+             'permissions' => NULL,
+             'datatype' => 'data',
+             'relationType' => false,
+             'invisible' => false,
+             'visibleGridView' => false,
+             'visibleSearch' => false,
+          )),
+        ),
+         'locked' => false,
+      )),
+    ),
+     'locked' => false,
+  )),
+   'icon' => '',
+   'previewUrl' => '',
+   'group' => '',
    'showAppLoggerTab' => false,
-   'linkGeneratorReference' => NULL,
+   'linkGeneratorReference' => '',
    'propertyVisibility' => 
   array (
     'grid' => 
     array (
       'id' => true,
+      'key' => false,
       'path' => true,
       'published' => true,
       'modificationDate' => true,
@@ -45,6 +154,7 @@ return Pimcore\Model\DataObject\ClassDefinition::__set_state(array(
     'search' => 
     array (
       'id' => true,
+      'key' => false,
       'path' => true,
       'published' => true,
       'modificationDate' => true,
