b375f86cec395429475fc62f4cbd1bbc58767964
_Andry <andryan.hartopo@salt.co.id>
2019-01-11 16:35:57 +0700

    salah masukin

--- docroot/app/Resources/views/Areas/bfi-widget-payment/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-widget-payment/view.html.php
@@ -3,12 +3,14 @@
         <div class="payment-wrap">
             <h4><?= $this->input("method"); ?>:</h4>
             <div class="block-payment__item">
+                <div class="row">
                 <?php while ($this->block("contentblock-2")->loop()) { ?>
                     <?php $asset = $this->image("image");?>
-                    <div class="row">
+                    
                         <div class="col-md-4 col-sm-6"><a href="<?= $this->link('url')->getHref(); ?>"><img src="<?= $asset->getImage()?>" alt=""></a></div>
-                    </div>
+                   
                 <?php } ?>
+                </div>
             </div>
         </div>
     <?php } ?>
