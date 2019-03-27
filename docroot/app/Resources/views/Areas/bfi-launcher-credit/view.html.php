471a196050d5cb96afad928843e84e6291408a57
Kiky Adryan <kiky.adryan@salt.co.id>
2019-03-27 14:59:17 +0700

    add additional class

--- docroot/app/Resources/views/Areas/bfi-launcher-credit/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-launcher-credit/view.html.php
@@ -5,7 +5,10 @@
             <h2 class="text-center">
                 <?= $this->input('title');?>
                 <?php $asset = $this->image("image");?>
-                <a href="<?= $this->link('url')->getHref(); ?>" class="cta cta-primary cta-big"><?= $this->input('button');?></a>
+                <a href="<?= $this->link('url')->getHref(); ?>" 
+					class="cta cta-primary cta-big <?= $this->link('url')->getClass();?>">
+					<?= $this->input('button');?>
+				</a>
             </h2>
         </div>
     </div>
