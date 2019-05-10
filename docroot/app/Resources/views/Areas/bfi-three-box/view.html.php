7d9b5097b18bba7ab04ae87cbe4202a9a5c4d10a
Kiky Adryan <kiky.adryan@salt.co.id>
2019-05-10 16:22:41 +0700

    Add class to open the chat

--- docroot/app/Resources/views/Areas/bfi-three-box/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-three-box/view.html.php
@@ -1,15 +1,15 @@
 <div>
-    <?php $asset = $this->image("image");?>
-    <div id="three-box" class="three-box" style="background-image: url('<?= $asset->getImage()?>')">
+    <?php $asset = $this->image("image"); ?>
+    <div id="three-box" class="three-box" style="background-image: url('<?= $asset->getImage() ?>')">
         <div class="container">
             <div class="row">
                 <?php while ($this->block("contentblock")->loop()) { ?>
-                    <?php $asset = $this->image("image");?>
+                    <?php $asset = $this->image("image"); ?>
                     <div class="col-md-4 col-sm-4 textalign-center">
                         <div class="three-box__item">
-                            <h4><?= $this->input('title');?></h4>
-
-                            <a href="<?= $this->link('url')->getHref(); ?>">
+                            <h4><?= $this->input('title'); ?></h4>
+                            <a class="<?= $this->link('url')->getClass(); ?>"
+                               href="<?= $this->link('url')->getHref(); ?>">
                                 <?= $this->link('url')->getText(); ?>
                             </a>
                         </div>
