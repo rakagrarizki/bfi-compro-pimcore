115b1f48e8b3fd9df98cc0c400c9472a5a98faa1
Asqolani <asqolani.addausi@erasysconsulting.com>
2019-12-02 11:48:09 +0700

    Develop Widget list-title, object-popup

--- docroot/app/Resources/views/Areas/bfi-report-list-icon-year/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-report-list-icon-year/view.html.php
@@ -11,3 +11,55 @@ $reports->addConditionParam("Category__id = ?",$category,"AND");
 <?= $data->get?>
 
 <?php endforeach;?>
+
+
+<!-- Template -->
+<div class="container">
+    <ul class="card-list-box">
+        <li class="card-list">
+            <img src="/static/images/people.jpg" alt="">
+            <div class="card-content">
+                <h6>2018</h6>
+                <h3>Embracing a New Normal</h3>
+                <a href="#">
+                    <img src="image/logo/download.png" alt="">
+                    <span><u>Unduh Document</u></span>
+                </a>
+            </div>
+        </li>
+        <li class="card-list">
+            <img src="image/content-image/card-image.png" alt="">
+            <div class="card-content">
+                <h6>2017</h6>
+                <h3>Grow Leads, Create Value</h3>
+                <a href="#">
+                    <img src="image/logo/download.png" alt="">
+                    <span><u>Unduh Document</u></span>
+                </a>
+            </div>
+        </li>
+        <li class="card-list">
+            <img src="image/content-image/card-image.png" alt="">
+            <div class="card-content">
+                <h6>2016</h6>
+                <h3>Innovate to Serve</h3>
+                <a href="#">
+                    <img src="image/logo/download.png" alt="">
+                    <span><u>Unduh Document</u></span>
+                </a>
+            </div>
+        </li>
+        <li class="card-list">
+            <img src="image/content-image/card-image.png" alt="">
+            <div class="card-content">
+                <h6>2015</h6>
+                <h3>Optimize - Grow - Lead</h3>
+                <a href="#">
+                    <img src="image/logo/download.png" alt="">
+                    <span><u>Unduh Document</u></span>
+                </a>
+            </div>
+        </li>
+    </ul>
+</div>
+<!-- Template -->
