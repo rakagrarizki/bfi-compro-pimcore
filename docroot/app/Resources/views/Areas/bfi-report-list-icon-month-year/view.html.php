b775a9b5fab7130e9d3244406eb25404100040aa
Alini Tjin <alini@salt.co.id>
2019-12-02 18:57:30 +0700

    integrate FE slicing with Be data

--- docroot/app/Resources/views/Areas/bfi-report-list-icon-month-year/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-report-list-icon-month-year/view.html.php
@@ -5,62 +5,26 @@ $reports = new \Pimcore\Model\DataObject\Report\Listing();
 $reports->addConditionParam("Category__id = ?",$category,"AND");
 
 ?>
-<?php foreach($reports as $data):?>
-<?= $data->getFilename();?>
-<?= $data->getUrl();?>
-<?= $data->get?>
 
-
-<?php endforeach;?>
 
 
 <!-- Template -->
 <div class="container">
     <ul class="card-list-box">
+        <?php foreach($reports as $data):?>
         <li class="card-list">
             <img src="image/content-image/card-image.png" alt="">
             <div class="card-content">
-                <h6>Januari 2018</h6>
-                <h3>Mandiri Sekuritas</h3>
-                <a href="#">
+                <h6><?= $data->getDate()->formatLocalized("%B %Y")?></h6>
+                <h3><?= $data->getFilename();?></h3>
+                <a href="<?= $data->getUrl();?>">
                     <img src="image/logo/download.png" alt="">
-                    <span><u>Unduh Document</u></span>
+                    <span><u><?= $this->t("download-document")?></u></span>
                 </a>
             </div>
         </li>
-        <li class="card-list">
-            <img src="image/content-image/card-image.png" alt="">
-            <div class="card-content">
-                <h6>Februari 2018</h6>
-                <h3>CLSA</h3>
-                <a href="#">
-                    <img src="image/logo/download.png" alt="">
-                    <span><u>Unduh Document</u></span>
-                </a>
-            </div>
-        </li>
-        <li class="card-list">
-            <img src="image/content-image/card-image.png" alt="">
-            <div class="card-content">
-                <h6>Maret 2018</h6>
-                <h3>DBS Vickers</h3>
-                <a href="#">
-                    <img src="image/logo/download.png" alt="">
-                    <span><u>Unduh Document</u></span>
-                </a>
-            </div>
-        </li>
-        <li class="card-list">
-            <img src="image/content-image/card-image.png" alt="">
-            <div class="card-content">
-                <h6>April 2018</h6>
-                <h3>CLSA</h3>
-                <a href="#">
-                    <img src="image/logo/download.png" alt="">
-                    <span><u>Unduh Document</u></span>
-                </a>
-            </div>
-        </li>
+        <?php endforeach;?>
+
     </ul>
 </div>
 <!-- Template -->
