57ccfc7e8850c6dedc2f9906d61a0c03631eb2e9
muhyidinamin <m.amin@erasysconsulting.com>
2019-12-05 12:04:30 +0700

    update responsive content

--- docroot/web/Core/script/Includes/contact-us.js
+++ docroot/web/Core/script/Includes/contact-us.js
@@ -1,4 +1,8 @@
 (function ($) {
+    $('select').on('change', function (e) {
+        $('label[for="type_message"]').css('visibility', 'visible');
+    });
+
     $('select.type-message').on('change', function(){
         console.log($(this).children("option:selected"). val())
         $('.select2-selection__rendered').css('line-height', '45px');
@@ -10,12 +14,9 @@
 
     $('input[type="radio"]#type').on('change', function(){
         var value = $("input[name='tipe_nasabah']:checked").val();
-        console.log(value);
         if (value === 'nasabah'){
-            console.log(value);
             $('input#no_kontrak').prop( "disabled", false );
         }else if (value === 'non-nasabah'){
-            console.log(value);
             $('input#no_kontrak').prop( "disabled", true );
         }
     })
