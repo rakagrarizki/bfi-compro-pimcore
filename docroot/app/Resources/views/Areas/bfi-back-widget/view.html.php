9412a065a5b46a31cfb6c301d5f34a8cc73635ae
imamdnl <imamdnl11@gmail.com>
2019-12-11 13:54:21 +0700

    integrate share-widget

--- docroot/app/Resources/views/Areas/bfi-back-widget/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-back-widget/view.html.php
@@ -1,1 +1,9 @@
-<a href="javascript:history.back()">Go Back</a>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <div class="container">
+            <li class="breadcrumb-item active cta cta-back" aria-current="page">
+                <a href="javascript:history.back()"> <i class="cta"></i> Kembali ke Halaman Sebelumnya</a>
+            </li>
+        </div>
+    </ol>
+</nav>
