d9235db20af0ae8e3c3384bae4f9b377b8e4a651
imamdnl <imamdnl11@gmail.com>
2019-12-11 17:22:22 +0700

    Revert "integrate share widget"
    This reverts commit 38d834bfff471b2a584b587d0ced8612851612bd.

--- docroot/app/Resources/views/Areas/bfi-share-widget/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-share-widget/view.html.php
@@ -4,19 +4,16 @@ $titleshare = $this->input("title");
 $lang = $this->getLocale();
 $asset = $this->image("Image");
 //$fixedurl = $this->link("url")->getHref();
-// $fixedurl = BASEURL . $this->document->getFullPath();
-$fixedurl = $this->link("url")->getHref() . $this->document->getFullPath();
+$fixedurl = BASEURL . $this->document->getFullPath();
 $imagethumbnail = $asset->getImage();
 //$urlcheck = Pimcore\Model\Asset::getByPath($imagethumbnail);
 $urlFacebook = " https://www.facebook.com/sharer/sharer.php?u=" . $fixedurl . "&title=" . $titleshare . "&picture=" . $imagethumbnail;
 $urlTwitter = " https://twitter.com/share?text=$titleshare&url=$fixedurl&wrap_links=true "; ?>
 
-<div class="container">
-    <div class="share">
-        <span>Share: </span>
-        <a href="<?= $urlFacebook; ?>" class="share-fb btn btn-info"><i class="fa fa-facebook"></i></a>
-        <a href="<?= $urlTwitter; ?>" class="share-tw btn btn-info"><i class="fa fa-twitter"></i></a>
-        <a class="share-cp btn btn-info" id="copy" onclick="copyURL('<?= $fixedurl ?>')"><i class="fa fa-chain"></i></a>
-        <div style="display: none;padding: 0px 20px;" id="copied">Copied</div>
-    </div>
+<div class="share">
+    <span>Share:</span>
+    <a href="<?= $urlFacebook;?>" class="share-fb"><i class="fa fa-facebook"></i></a>
+    <a href="<?= $urlTwitter;?>" class="share-tw"><i class="fa fa-twitter"></i></a>
+    <a class="share-cp" id="copy" onclick="copyURL('<?= $fixedurl ?>')"><i class="fa fa-chain"></i></a>
+    <div style="display: none;padding: 0px 20px;" id="copied">Copied</div>
 </div>
