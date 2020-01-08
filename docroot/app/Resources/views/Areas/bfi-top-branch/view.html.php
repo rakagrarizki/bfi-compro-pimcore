f038c91db4d52d235c6d711cd417f8c7ea7ae788
Asqolani <asqolani.addausi@erasysconsulting.com>
2020-01-08 19:49:48 +0700

    fixing based on QC.ppt progress 40%

--- docroot/app/Resources/views/Areas/bfi-top-branch/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-top-branch/view.html.php
@@ -1,12 +1,12 @@
-<section id="bfi-cabang">
+<!-- <section id="bfi-cabang">
     <div class="container">
         <h2 class="title-wrapper text-center">
-            <?= $this->input('title');?>
+            </?= $this->input('title');?>
         </h2>
-        <p class="text-center paragraf-title"><?= $this->textarea('text');?></p>
+        <p class="text-center paragraf-title"></?= $this->textarea('text');?></p>
 
         <div class="row content-bfi">
-            <?php
+            </?php
             $i = 0;
             foreach ($this->branch as $branch){
                 if($i>3){
@@ -24,12 +24,12 @@
                 <div class="col-md-4 thumbnail thumbnail--branch">
                     <img src="/static/images/icon/branch1.png">
                     <div class="thumbnail-caption">
-                        <h3><?= $name; ?></h3>
-                        <p><?= $branch->getAddress(); ?></p>
-                        <a href="/<?= $this->getLocale() ?>/branch-office?longitude=<?= $branch->getMap() ? $branch->getMap()->getLongitude() : '' ?>&latitude=<?= $branch->getMap() ? $branch->getMap()->getLatitude() : '' ?>" class="cta-location"><?= $this->translate("seeLocation") ?></a>
+                        <h3></?= $name; ?></h3>
+                        <p></?= $branch->getAddress(); ?></p>
+                        <a href="/</?= $this->getLocale() ?>/branch-office?longitude=</?= $branch->getMap() ? $branch->getMap()->getLongitude() : '' ?>&latitude=</?= $branch->getMap() ? $branch->getMap()->getLatitude() : '' ?>" class="cta-location"></?= $this->translate("seeLocation") ?></a>
                     </div>
                 </div>
-                <?php
+                </?php
                 $i++;
             }
             ?>
@@ -37,12 +37,29 @@
         </div>
 
         <div class="jamoperasional">
-            <?php echo $this->wysiwyg('value', ["customConfig" => "custom/ckeditor_config.js"]) ?>
+            </?php echo $this->wysiwyg('value', ["customConfig" => "custom/ckeditor_config.js"]) ?>
         </div>
 
         <div class="button-area text-center">
-            <a href="<?= $this->link('url')->getHref(); ?>" class="cta cta-primary cta-big cta-see"><?= $this->translate("more") ?></a>
+            <a href="</?= $this->link('url')->getHref(); ?>" class="cta cta-primary cta-big cta-see"></?= $this->translate("more") ?></a>
         </div>
 
+    </div>
+</section> -->
+
+<section id="bfi-branch">
+    <div class="container">
+        <div class="title">
+            <h1>BFI Hadir di Lebih Dari 400 Lokasi Pelayanan di Seluruh Indonesia</h1>
+            <p>Cari cabang atau gerai terdekat Anda</p>
+        </div>
+        <div class="button">
+            <a href="#"><span>SELENGKAPNYA</span><i class="fa fa-angle-right"></i></a>
+        </div>
+        <div class="operation-time">
+            <p class="info">Jam Operasional :</p>
+            <p class="date-time">Senin-Jumat 08.00-17.00 WIB,</p>
+            <p class="date-time">Sabtu 08.00-15.30 WIB,</p>
+        </div>
     </div>
 </section>
