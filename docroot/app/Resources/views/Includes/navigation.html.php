0db9a2f38b85ce17f7e0355f5f4c934b4674946a
Daniel <daniel.imam@salt.co.id>
2020-01-09 13:21:01 +0700

    fix navigation and language

--- docroot/app/Resources/views/Includes/navigation.html.php
+++ docroot/app/Resources/views/Includes/navigation.html.php
@@ -10,9 +10,37 @@ use Pimcore\Model\Document;
 use Pimcore\Model\Document\Page;
 ?>
 <?php $pageCurrent = $this->getParam('page', 1); ?>
+<?php $lang = $this->getLocale(); ?>
 <nav id="site-header">
     <div class="navbar-fixed-top hidden-xs">
-        <?= $this->inc("/" . $this->getLocale() . "/shared/includes/sub-navigation") ?>
+
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 left-side-top">
+                        <a class="_personal" href="/<?php echo $this->getLocale() ?>"><?= $this->translate("personal") ?></a>
+                        <a class="_grup" href="<?= "/" . $lang . "/corporate" ?>">
+                            <?= $this->translate("corporate") ?></a>
+                    </div>
+                    <div class="col-md-6 col-sm-6 right-side-top clearfix">
+                        <div class="link-about-top">
+                            <a href="<?= "/" . $lang . "/tentang-kami" ?>">
+                                <?= $this->translate("tentang-kami") ?></a>
+                            <a href="<?= "/" . $lang . "/blog" ?>">
+                                <?= $this->translate("blog") ?></a>
+                        </div>
+
+                        <div class="link-log">
+                            <a href="#" class="login"><?= $this->translate("login") ?></a>
+                            <!-- <a href="#" class="register"><?/*= $this->translate("register") */ ?></a> -->
+                            <?php echo $this->template("Includes/language.html.php") ?>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
         <div class="header-bottom">
             <div class="container">
                 <div class="row">
@@ -31,29 +59,27 @@ use Pimcore\Model\Document\Page;
                                 $subPage = $this->navigation()->buildNavigation($this->document, $listMenu);
                                 //dump($subPage);exit;
 
-                                    if ($subPage ) {
+                                if ($subPage) {
                                     foreach ($subPage as $page) {
                                         $hasChildren = $page->hasPages();
 
                                         if (strpos($page->getUri(), 'branch-office') !== false) {
                                             continue;
                                         }
-                                        if($page->getHref() == "/".$this->getLocale()."/corporate"){
+                                        if ($page->getHref() == "/" . $this->getLocale() . "/corporate") {
                                             break;
                                         }
                                         if ($hasChildren && (strpos($page->getUri(), '#product') !== false)) {
-                                            ?>
+                                ?>
 
-                                            <li class="dropdown"
-                                                id="produk" onmouseover="hoverDropdown()" onmouseout="closeDropdown()">
-                                                <a href="#"
-                                                   class="<?php echo $page->getActive() ? 'active' : '' ?> produk"><?= $page->getLabel() ?></a>
+                                            <li class="dropdown" id="produk" onmouseover="hoverDropdown()" onmouseout="closeDropdown()">
+                                                <a href="#" class="<?php echo $page->getActive() ? 'active' : '' ?> produk"><?= $page->getLabel() ?></a>
                                                 <div class="dropdown-content main">
                                                     <div class="produk-hover container">
                                                         <div class="">
                                                             <?php
                                                             foreach ($page->getPages() as $child) {
-                                                                ?>
+                                                            ?>
                                                                 <div class="col-md-6">
                                                                     <ul>
                                                                         <li>
@@ -65,56 +91,56 @@ use Pimcore\Model\Document\Page;
 
                                                                         if ($hasGrandChildren) {
                                                                             foreach ($child->getPages() as $grandChild) {
-                                                                                ?>
-                                                                                <?php if($grandChild->getDocumentType() != "link"):?>
-                                                                                <li>
-                                                                                    <a class="<?php echo $grandChild->getActive() ? 'active' : '' ?>" href="<?= $grandChild->getHref() ?>"><?= $grandChild->getLabel() ?></a>
-                                                                                </li>
-                                                                                <?php else :?>
+                                                                        ?>
+                                                                                <?php if ($grandChild->getDocumentType() != "link") : ?>
+                                                                                    <li>
+                                                                                        <a class="<?php echo $grandChild->getActive() ? 'active' : '' ?>" href="<?= $grandChild->getHref() ?>"><?= $grandChild->getLabel() ?></a>
+                                                                                    </li>
+                                                                                <?php else : ?>
                                                                                     <li>
                                                                                         <div class="label-title"><?= $grandChild->getLabel(); ?></div>
                                                                                     </li>
-                                                                                <?php endif?>
+                                                                                <?php endif ?>
                                                                                 <?php
                                                                                 $hasGreatGrandChild = $grandChild->hasPages();
-                                                                                if($hasGreatGrandChild) {
-                                                                                    foreach($grandChild->getPages() as $greatGrandChild){
+                                                                                if ($hasGreatGrandChild) {
+                                                                                    foreach ($grandChild->getPages() as $greatGrandChild) {
                                                                                 ?>
                                                                                         <li>
                                                                                             <a class="<?php echo $greatGrandChild->getActive() ? 'active' : '' ?>" href="<?= $greatGrandChild->getHref() ?>"><?= $greatGrandChild->getLabel() ?></a>
                                                                                         </li>
-                                                                                <?php }
+                                                                        <?php }
                                                                                 }
                                                                             }
                                                                         }
                                                                         ?>
                                                                     </ul>
                                                                 </div>
-                                                                <?php
+                                                            <?php
                                                             }
                                                             ?>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </li>
-                                            <?php
+                                        <?php
                                         } else {
-                                            ?>
+                                        ?>
                                             <li class="<?php echo $page->getActive() ? 'active' : '' ?>">
                                                 <a href="<?= $page->getHref() ?>">
                                                     <?= $page->getLabel() ?>
                                                 </a>
                                             </li>
-                                            <?php
+                                <?php
                                         }
                                     }
                                 }
 
                                 ?>
-                                
+
                             </ul>
                             <div class="search-button">
-                                <a href=""><i class="fa fa-search"></i></a>
+                                <a href="<?= "/" . $lang . "/search" ?>"><i class="fa fa-search"></i></a>
                             </div>
                         </div>
 
