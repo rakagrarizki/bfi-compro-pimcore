80a1e67b5c3b8d0e605501049f4668e16610c8fc
Daniel <daniel.imam@salt.co.id>
2020-01-16 10:58:08 +0700

    fix next button not shown at last page

--- docroot/app/Resources/views/Includes/paging.html.php
+++ docroot/app/Resources/views/Includes/paging.html.php
@@ -1,30 +1,32 @@
-<?php $p = $this->getParam("page");?>
+<?php $p = $this->getParam("page"); ?>
 <nav aria-label="Page navigation">
     <ul class="pagination">
         <?php
         if (isset($this->previous)) {
         ?>
-        <li>
-            <a href="<?= urldecode($this->pimcoreUrl(['page' => $this->previous])); ?>" aria-label="Previous">
-                <i class="fa fa-angle-left"></i>
-            </a>
-        </li>
+            <li>
+                <a href="<?= urldecode($this->pimcoreUrl(['page' => $this->previous])); ?>" aria-label="Previous">
+                    <i class="fa fa-angle-left"></i>
+                </a>
+            </li>
         <?php
         }
         ?>
-        <?php foreach ($this->pagesInRange as $page): ?>
+        <?php foreach ($this->pagesInRange as $page) : ?>
 
-            <?php if ($page == $p): ?>
+            <?php if ($page == $p) : ?>
 
-            <li class="active"><a href="javascript:void(0)"><?= $page; ?></a></li>
-            <?php else :?>
-            <li><a href="<?= $this->pimcoreUrl(['page' => $page]); ?>"><?= $page; ?></a></li>
-            <?php endif;?>
-        <?php endforeach;?>
-        <li>
-            <a href="<?= $this->pimcoreUrl(['page' => $this->next]); ?>" aria-label="Next">
-                <i class="fa fa-angle-right"></i>
-            </a>
-        </li>
+                <li class="active"><a href="javascript:void(0)"><?= $page; ?></a></li>
+            <?php else : ?>
+                <li><a href="<?= $this->pimcoreUrl(['page' => $page]); ?>"><?= $page; ?></a></li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+        <?php if (isset($this->next)) { ?>
+            <li>
+                <a href="<?= $this->pimcoreUrl(['page' => $this->next]); ?>" aria-label="Next">
+                    <i class="fa fa-angle-right"></i>
+                </a>
+            </li>
+        <?php } ?>
     </ul>
 </nav>
