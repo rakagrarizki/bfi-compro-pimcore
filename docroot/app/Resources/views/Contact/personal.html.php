f31fce7912c4d6675f9ef0589d3fcbde7fe3738f
muhyidinamin <m.amin@erasysconsulting.com>
2020-01-20 14:45:58 +0700

    update bug fix case 100 dan 101

--- docroot/app/Resources/views/Contact/personal.html.php
+++ docroot/app/Resources/views/Contact/personal.html.php
@@ -108,7 +108,7 @@ $this->extend('layout.html.php');
                         <span>Max. ukuran file adalah 500kb</span>
                     </div>
                     <div class="form-group captcha">
-                        <div class="g-recaptcha" id="g-recaptcha" data-sitekey="6LcsxcUUAAAAAO22We2mizM6KrBMmPECFMVMJ4NE"></div>
+                        <div class="g-recaptcha" id="g-recaptcha" data-sitekey="6Ldb1c8UAAAAALZP6FbNJAM7z8T-tPpkbJvjbJBW"></div>
                     </div>
                 </div>
                 <div class="button-area text-center">
