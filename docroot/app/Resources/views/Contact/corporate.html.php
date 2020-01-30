86f39e7ba2a44632e737184ed46875eaeffe7154
Daniel <daniel.imam@salt.co.id>
2020-01-30 14:46:44 +0700

    add translation

--- docroot/app/Resources/views/Contact/corporate.html.php
+++ docroot/app/Resources/views/Contact/corporate.html.php
@@ -25,33 +25,33 @@ $this->extend('layout.html.php');
                     <?php if ($this->msg_error) {
                         echo '<div class="alert alert-danger" role="alert">' . $msg_error . '</div>';
                     } ?>
-                    <h2 class="text-center">Hubungi Kami</h2>
-                    <p class="text-center">Silahkan mengisi form dibawah ini, agen BFI akan menghubungi Anda</p>
+                    <h2 class="text-center"><?= $this->translate('contact'); ?></h2>
+                    <p class="text-center"><?= $this->translate('contact-sub'); ?></p>
                 </div>
 
                 <div class="form-group">
                     <label for="corporate[name]"><?= $this->translate('form-name') ?></label>
-                    <input type="text" class="form-control formRequired formName" name="corporate[name]" id="nama_lengkap" placeholder="Masukkan nama lengkap Anda">
+                    <input type="text" class="form-control formRequired formName" name="corporate[name]" id="nama_lengkap" placeholder="<?= $this->translate('placeholder-name') ?>">
                     <div class="error-wrap"></div>
                 </div>
                 <div class="form-group">
                     <label for="corporate[phone]"><?= $this->translate('form-hp') ?></label>
-                    <input type="text" class="form-control formRequired formPhoneNumber" name="corporate[phone]" id="no_handphone" maxlength="13" placeholder="Masukkan nomor handphone Anda">
+                    <input type="text" class="form-control formRequired formPhoneNumber" name="corporate[phone]" id="no_handphone" maxlength="13" placeholder="<?= $this->translate('placeholder-phone') ?>">
                     <div class="error-wrap"></div>
                 </div>
                 <div class="form-group">
                     <label for="corporate[email]"><?= $this->translate('form-email') ?></label>
-                    <input type="email" class="form-control formEmail" name="corporate[email]" id="email_penanya" placeholder="Masukkan email Anda">
+                    <input type="email" class="form-control formEmail" name="corporate[email]" id="email_penanya" placeholder="<?= $this->translate('placeholder-email') ?>">
                     <div class="error-wrap"></div>
                 </div>
                 <div class="form-group">
                     <label for="corporate[subject]"><?= $this->translate('Perihal') ?></label>
-                    <input type="text" class="form-control formRequired formPerihal" name="corporate[subject]" id="perihal" maxlength="20" placeholder="Perihal">
+                    <input type="text" class="form-control formRequired formPerihal" name="corporate[subject]" id="perihal" maxlength="20" placeholder="<?= $this->translate('Perihal') ?>">
                     <div class="error-wrap"></div>
                 </div>
                 <div class="form-group">
                     <label for="message"><?= $this->translate('Pesan') ?></label>
-                    <textarea class="form-control formRequired formMessage" name="corporate[message]" id="message" placeholder="Masukkan Pesan Anda"></textarea>
+                    <textarea class="form-control formRequired formMessage" name="corporate[message]" id="message" placeholder="<?= $this->translate('placeholder-pesan') ?>"></textarea>
                     <div class="error-wrap"></div>
                 </div>
                 <div class="form-group captcha">
@@ -59,7 +59,7 @@ $this->extend('layout.html.php');
                 </div>
             </div>
             <div class="button-area text-center">
-                <button class="cta cta-primary cta-big" type="submit">Kirim Pesan</button>
+                <button class="cta cta-primary cta-big" type="submit"><?= $this->translate('submit-contact'); ?></button>
             </div>
         </form>
         <!-- </div> -->
