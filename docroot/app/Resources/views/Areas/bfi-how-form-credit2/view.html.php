371efdd8ab2a927fa99abd5d36e4601ff9d57fef
kurtzawn <kurtz.awn@gmail.com>
2020-02-03 16:27:07 +0700

    update ajukan pembiayaan

--- docroot/app/Resources/views/Areas/bfi-how-form-credit2/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-how-form-credit2/view.html.php
@@ -68,38 +68,22 @@
 <section id="pengajuan">
     <div class="container">
         <div class="submission-wrapper">
-            <!-- <h4>Simulasikan Pengajuan Kredit Anda</h4> -->
+
             <h4><?= $this->input('title'); ?></h4>
             <form action="" id="selection-form">
                 <div class="selection-wrapper">
                     <div class="selection-1">
-                        <select id="category-1">
-                            <option value="" selected style="display:none;"></option>
-                            <?php while ($this->block("contentblock")->loop()) {
-                                $url = !$this->link('url')->isEmpty() ? $this->link('url')->getHref() : ""; ?>
-                                <option value="<?= $this->input('value') ?>" id="block-<?= $this->block('contentblock')->getCurrent() ?>"><?= $this->input('text'); ?></option>
-                            <?php } ?>
-                            <!-- <option value="1">BPKB Motor</option>
-                            <option value="2">BPKB Mobil</option> -->
+                        <select id="category-1"class="c-custom-select-home form-control" placeholder="<?= $this->translate('Pilih jenis pembiayaan')?>" >
+                             <option value="" selected style="display:none;" disabled selected><?= $this->translate('Pilih jenis pembiayaan') ?></option>
                         </select>
                     </div>
                     <div class="selection-2">
-                        <select id="category-2">
-                            <option value="" selected style="display:none;"></option>
-                            <?php while ($this->block("contentblock")->loop()) {
-                                $id = $this->block('contentblock')->getCurrent();
-                                while ($this->block("contentblock1")->loop()) {
-                                    $url = !$this->link('url')->isEmpty() ? $this->link('url')->getHref() : ""; ?>
-                                    <option value="<?= $url ?>" class="block-<?= $id ?>"><?= $this->input('text1'); ?></option>
-                            <?php }
-                            } ?>
-                            <!-- <option value="1">BPKB Motor2</option>
-                            <option value="2">BPKB Mobil2</option> -->
+                    <select id="category-2" class="form-control formRequired" name="category-2" placeholder="<?= $this->translate('Pembiayaan apa yang dibutuhkan?') ?>">
+                             <option value="" selected style="display:none;" disabled selected><?= $this->translate('Pembiayaan apa yang dibutuhkan?') ?></option>
                         </select>
                     </div>
                 </div>
-                <!-- <button class="btn-next">Hitung &nbsp<i class="fa fa-angle-right"></i></button> -->
-                <button class="btn-next"><?= $this->translate('ajukan-sekarang'); ?><i class="fa fa-angle-right"></i></button>
+                <button class="btn-next"><?= $this->translate('ajukan-sekarang'); ?><i class="fa fa-angle-right" type="submit"></i></button>
             </form>
         </div>
     </div>
