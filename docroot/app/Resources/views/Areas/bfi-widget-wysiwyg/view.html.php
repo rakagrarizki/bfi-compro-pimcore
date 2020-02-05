e6abd4f017c1116395716fc1d8d75f9ad3db10f5
kurtzawn <kurtz.awn@gmail.com>
2020-02-05 15:42:52 +0700

    update wysiwyg font and table ul

--- docroot/app/Resources/views/Areas/bfi-widget-wysiwyg/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-widget-wysiwyg/view.html.php
@@ -5,3 +5,15 @@
         </div>
     </div>
 </div>
+
+<style type="text/css">
+ span{
+     font-family:"Helvetica Neue";
+ }
+ li::before{
+    content: '\2022';
+ }
+ .MsoTableGrid ul{
+    text-align: left;
+ }
+ </style>
