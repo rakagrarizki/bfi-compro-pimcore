500023575d0a8503307a64d1d6b8e732368fe955
Daniel <daniel.imam@salt.co.id>
2020-02-18 14:55:12 +0700

    fix widget blog-news and corporate-news

--- docroot/src/AppBundle/Document/Areabrick/BfiBlogNews.php
+++ docroot/src/AppBundle/Document/Areabrick/BfiBlogNews.php
@@ -9,20 +9,11 @@
 namespace AppBundle\Document\Areabrick;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\Document\Tag\Area\Info;
-use Symfony\Component\HttpFoundation\Request;
 
 class BfiBlogNews extends AbstractAreabrick
 {
     public function action(Info $info){
-        $request = Request::createFromGlobals();
-        
-        $page = $request->server->get('REQUEST_URI');
-
-        if ($page == "/" || $page == "/en" || $page == "/id") {
-            $news = new DataObject\BlogArticle\Listing();
-        } else {
-            $news = new DataObject\News\Listing();
-        }
+        $news = new DataObject\BlogArticle\Listing();
 
         $news->setLimit(3);
         $info->getView()->news = $news;
