555bd1adeaf31cb8e48ca5e09d4e8bad1d7b8223
kurtzawn <kurtz.awn@gmail.com>
2020-02-24 19:04:49 +0700

    update 17 issues fix

--- docroot/app/Resources/views/Areas/bfi-small-image-left-wysiwyg-right/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-small-image-left-wysiwyg-right/view.html.php
@@ -8,10 +8,10 @@
 
 <div class="container wysiwyg-list-right hidden-xs">
     <div class="row">
-        <div class="col-lg-4 col-md-4 col-12">
+        <div class="col-lg-4 col-md-4 col-12 desktop-img">
             <img src="<?= $asset->getImage()?>">
         </div>
-        <div class="col-lg-8 col-md-8 col-12">
+        <div class="col-lg-8 col-md-8 col-12 desktop-details">
             <?= $this->wysiwyg("text"); ?>
         </div>
     </div>
@@ -19,14 +19,14 @@
 
 <!-- Mobile view -->
 
-<div class="container wysiwyg-list-right hidden-md hidden-lg hidden-sm hidden-md">
+<div class="container wysiwyg-list-right mobile-view hidden-md hidden-lg hidden-sm hidden-md">
     <div class="row">
-    <div class="col-12">
+    <div class="col-12 img-responsive img-mobile-view">
             <img src="<?= $asset->getImage()?>">
         </div>
     </div>
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 details">
             <?= $this->wysiwyg("text"); ?>
         </div>
     </div>
