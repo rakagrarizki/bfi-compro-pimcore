6f09174a64507230f8f692723b880d203187b405
Daniel <daniel.imam@salt.co.id>
2020-03-05 15:05:58 +0700

    revert href at widget corporate-news

--- docroot/app/Resources/views/Areas/bfi-corporate-news/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-corporate-news/view.html.php
@@ -44,7 +44,7 @@ $news->load();
                             $date = date("d.m.y", $dateUnix);
                             ?>
                             <p><?= $data->getCategory()->getName(); ?></p>
-                            <h3><a href="/<?= $lang; ?>/news/<?= $data->getSlug(); ?>"><?= $data->getTitle();?></a></h3>
+                            <h3><a href="/<?= $this->getLocale(); ?>/news/<?= $data->getSlug(); ?>"><?= $data->getTitle();?></a></h3>
                             <p class="date"><?= $date; ?> | <i class="fa fa-eye"></i> <?= $data->getViews(); ?></p>
                         </div>
                     </div>
