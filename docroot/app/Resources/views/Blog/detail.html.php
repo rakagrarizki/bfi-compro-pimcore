fc26785936d513fb2ca93ef3fffe4d8298797e8a
Daniel <daniel.imam@salt.co.id>
2020-03-10 13:10:31 +0700

    add target:"_blank" for facebook and twitter share on page blog,news and promo

--- docroot/app/Resources/views/Blog/detail.html.php
+++ docroot/app/Resources/views/Blog/detail.html.php
@@ -57,8 +57,8 @@ $urlTwitter = " https://twitter.com/share?text=$titleshare&url=$fixedurl&wrap_li
             </div>
             <div class="share">
                 <span>Share:</span>
-                <a href="<?= $urlFacebook;?>" class="share-fb"><i class="fa fa-facebook"></i></a>
-                <a href="<?= $urlTwitter;?>" class="share-tw"><i class="fa fa-twitter"></i></a>
+                <a href="<?= $urlFacebook;?>" class="share-fb" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="<?= $urlTwitter;?>" class="share-tw" target="_blank"><i class="fa fa-twitter"></i></a>
                 <a class="share-cp" id="copy" onclick="copyURL('<?= $fixedurl ?>')"><i class="fa fa-chain"></i></a>
                 <div style="display: none;padding: 0px 20px;" id="copied">Copied</div>
             </div>
