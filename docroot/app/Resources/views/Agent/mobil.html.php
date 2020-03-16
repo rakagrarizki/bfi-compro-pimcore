5fe4a37925c64e4777cfb58a7d40644a7417d50e
Daniel <daniel.imam@salt.co.id>
2020-03-16 14:51:35 +0700

    fix calling template file when success submit form agent

--- docroot/app/Resources/views/Agent/mobil.html.php
+++ docroot/app/Resources/views/Agent/mobil.html.php
@@ -508,7 +508,7 @@ $this->headScript()->offsetSetFile(101, '/static/js/Includes/general-form.js');
                         <h3></?= $this->translate('tq-text-1') ?></h3>
                         <p></?= $this->translate('tq-text-2') ?></p>
                     </div> -->
-                    <?= $this->template('Includes/request-otp.html.php'); ?>
+                    <?= $this->template('Agent/success.html.php'); ?>
                     <div class="button-area text-center backtohome">
                         <button class="cta cta-primary cta-big cta-see buttonnext backtohome" id="button7" 
                             type="button" onclick="return checkStatusPengajuan()"><?= $this->translate('backtohome') ?></button>
