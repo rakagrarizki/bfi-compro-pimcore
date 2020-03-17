47e013f128ce9649a8442f941bb0dab9c22d2fe3
kurtzawn <kurtz.awn@gmail.com>
2020-03-17 13:23:43 +0700

    redirect success and disabled btn safari

--- docroot/web/static/js/Includes/homepage.js
+++ docroot/web/static/js/Includes/homepage.js
@@ -190,6 +190,8 @@ $(document).ready(function() {
     changeSelected(this.value);
     $("#category-2").removeAttr("disabled");
   });
+
+  $("#category-2").prop( "disabled", false );
   
   $("#selection-form").validate({
     errorClass: "help-block",
