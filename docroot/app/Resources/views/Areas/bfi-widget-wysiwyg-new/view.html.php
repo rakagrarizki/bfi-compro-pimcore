a14e6c8692265b7a340799e1a80e5295754e9e7b
Daniel <daniel.imam@salt.co.id>
2020-03-18 14:03:30 +0700

    add new widget based on widget bfi-widget-wysiwyg

---
+++ docroot/app/Resources/views/Areas/bfi-widget-wysiwyg-new/view.html.php
@@ -0,0 +1,10 @@
+<div class="document-table" style="min-height:10px">
+    <div class="about-us-page">
+        <div class="container">
+            <div class="table-responsive">
+                <?php echo $this->wysiwyg('value', ["customConfig" => "custom/ckeditor_config.js"]) ?>
+            </div>
+        </div>
+    </div>
+</div>
+
