c140b33ad9eabcf3cae0996fd039c7f8423b242b
Asqolani <asqolani.addausi@erasysconsulting.com>
2020-03-19 17:48:49 +0700

    Fixing UAT Feedback Poin 310

--- docroot/app/Resources/views/Default/detail-transaksi.html.php
+++ docroot/app/Resources/views/Default/detail-transaksi.html.php
@@ -27,39 +27,41 @@ $this->extend('layout.html.php');
     </div>
 
     <div id="tabel-transaksi">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">No.</th>
-                    <th scope="col"><?= $this->translate('label-jatuh-tempo') ?></th>
-                    <th scope="col"><?= $this->translate('installment') ?></th>
-                    <th scope="col"><?= $this->translate('angsuran-dibayar') ?></th>
-                    <th scope="col"><?= $this->translate('tgl-bayar') ?></th>
-                    <th scope="col"><?= $this->translate('denda-terlambat') ?></th>
-                    <th scope="col"><?= $this->translate('sisa-hutang') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="installment_no"></td>
-                    <td class="tanggal_jatuh_tempo"></td>
-                    <td class="angsuran_per_bulan"></td>
-                    <td class="angsuran_telah_dibayar"></td>
-                    <td class="tanggal_pembayaran"></td>
-                    <td class="denda_keterlambatan"></td>
-                    <td class="sisa_angsuran"></td>
-                </tr>
-                <tr class="total">
-                    <td></td>
-                    <td><?= $this->translate('total-installment') ?></td>
-                    <td class="total_installment"></td>
-                    <td></td>
-                    <td></td>
-                    <td class="total_late_charge"></td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">No.</th>
+                        <th scope="col"><?= $this->translate('label-jatuh-tempo') ?></th>
+                        <th scope="col"><?= $this->translate('installment') ?></th>
+                        <th scope="col"><?= $this->translate('angsuran-dibayar') ?></th>
+                        <th scope="col"><?= $this->translate('tgl-bayar') ?></th>
+                        <th scope="col"><?= $this->translate('denda-terlambat') ?></th>
+                        <th scope="col"><?= $this->translate('sisa-hutang') ?></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="installment_no"></td>
+                        <td class="tanggal_jatuh_tempo"></td>
+                        <td class="angsuran_per_bulan"></td>
+                        <td class="angsuran_telah_dibayar"></td>
+                        <td class="tanggal_pembayaran"></td>
+                        <td class="denda_keterlambatan"></td>
+                        <td class="sisa_angsuran"></td>
+                    </tr>
+                    <tr class="total">
+                        <td></td>
+                        <td><?= $this->translate('total-installment') ?></td>
+                        <td class="total_installment"></td>
+                        <td></td>
+                        <td></td>
+                        <td class="total_late_charge"></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <div class="container">
