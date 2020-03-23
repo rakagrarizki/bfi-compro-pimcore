9131e74d282d75e383edfebf15696dffa13abd8b
kurtzawn <kurtz.awn@gmail.com>
2020-03-23 11:37:26 +0700

    update

--- docroot/web/Core/script/Includes/dashboard.js
+++ docroot/web/Core/script/Includes/dashboard.js
@@ -528,3 +528,16 @@ function applicationStatus(token, statusNumber, assignmentId) {
         }
     })
 }
+
+$('.modal-content').resizable({
+    //alsoResize: ".modal-dialog",
+    minHeight: 300,
+    minWidth: 300
+  });
+  $('.modal-dialog').draggable();
+
+  $('#myModal').on('show.bs.modal', function() {
+    $(this).find('.modal-body').css({
+      'max-height': '100%'
+    });
+  });
