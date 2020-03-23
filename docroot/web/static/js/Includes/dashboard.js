298bb84eeb8ae53d49bcf8c72c2078c5d56bb0c6
kurtzawn <kurtz.awn@gmail.com>
2020-03-23 14:13:19 +0700

    gulp 14:13

--- docroot/web/static/js/Includes/dashboard.js
+++ docroot/web/static/js/Includes/dashboard.js
@@ -528,16 +528,3 @@ function applicationStatus(token, statusNumber, assignmentId) {
         }
     })
 }
-
-$('.modal-content').resizable({
-    //alsoResize: ".modal-dialog",
-    minHeight: 300,
-    minWidth: 500
-  });
-  $('.modal-dialog').draggable();
-
-  $('#popup-ktp').on('show.bs.modal', function() {
-    $(this).find('.modal-body').css({
-      'max-height': '100%'
-    });
-  });
