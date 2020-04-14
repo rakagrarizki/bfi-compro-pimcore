458f64a304bfe893bdf0fbb993948523e48f4283
Daniel <daniel.imam@salt.co.id>
2020-04-14 11:32:21 +0700

    fix widget image left wysiwyg right

--- docroot/app/Resources/views/Areas/bfi-small-image-left-wysiwyg-right/edit.html.php
+++ docroot/app/Resources/views/Areas/bfi-small-image-left-wysiwyg-right/edit.html.php
@@ -10,7 +10,7 @@
     <div class="col-sm-12">
         <div class="row">
             <div class="col-sm-4">Title</div>
-            <div class="col-sm-8"><?= $this->input("Title"); ?></div>
+            <div class="col-sm-8"><?= $this->input("title"); ?></div>
         </div>
         <div class="row">
             <div class="col-sm-4">Image</div>
