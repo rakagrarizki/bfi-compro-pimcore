9066152aba22c93b50e6de9a4f8d2b1f755efba7
kurtzawn <kurtz.awn@gmail.com>
2020-04-27 13:12:45 +0700

    update class

--- docroot/app/Resources/views/Areas/bfi-link-widget/view.html.php
+++ docroot/app/Resources/views/Areas/bfi-link-widget/view.html.php
@@ -4,7 +4,7 @@
         <div class="down-box">
             <h3><?= $this->link('url')->getText(); ?></h3>
             <a href="<?= $this->link('url')->getHref(); ?>" target="<?= $this->link('url')->getTarget(); ?>">
-                <span class="cta cta-big cta-see full"><?= $this->t("more") ?></span>
+                <span class="cta cta-big cta-orange cta-see full"><?= $this->t("more") ?></span>
             </a>
         </div>
     </div>
