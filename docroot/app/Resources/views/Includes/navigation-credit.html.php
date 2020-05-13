5d54534013d5869d666abf97063f64eae0f0abac
Daniel <daniel.imam@salt.co.id>
2020-05-13 11:43:32 +0700

    fix navigation at form csr and success page

--- docroot/app/Resources/views/Includes/navigation-credit.html.php
+++ docroot/app/Resources/views/Includes/navigation-credit.html.php
@@ -9,6 +9,8 @@
 use Pimcore\Model\Document;
 use Pimcore\Model\Document\Page;
 
+$page = $_SERVER['REQUEST_URI'];
+$site = $this->document->getProperty("site");
 $lang = $this->getLocale();
 $name = "";
 if($_COOKIE["customer"] != "null") {
@@ -23,7 +25,13 @@ if($_COOKIE["customer"] != "null") {
             <div class="container">
                 <div class="row">
                     <div class="col-md-6 col-sm-6 left-side-top">
-                        <a href="/<?php echo $this->getLocale() ?>" class="backtohome"><?= $this->translate("backtohome") ?></a>
+                        <?php if ($site == "corporate") : ?>
+                            <?php if (preg_match("/.\/program-csr/", $page) || preg_match("/.\/CSR-Program/", $page)) { ?>
+                                <a href="/<?php echo $this->getLocale() ?>/corporate" class="backtohome"><?= $this->translate("backtohome") ?></a>
+                            <?php } ?>
+                        <?php else : ?>
+                            <a href="/<?php echo $this->getLocale() ?>" class="backtohome"><?= $this->translate("backtohome") ?></a>
+                        <?php endif; ?>
                     </div>
                     <div class="col-md-6 col-sm-6 right-side-top">
                     <div class="link-log">
@@ -31,7 +39,7 @@ if($_COOKIE["customer"] != "null") {
                             <!-- <div class="link-about-top">
                                 <a href="</?= "/" . $lang . "/tentang-kami" ?>">
                                     </?= $this->translate("tentang-kami") ?></a>
-                                <a href="<?= "/" . $lang . "/blog" ?>">
+                                <a href="</?= "/" . $lang . "/blog" ?>">
                                     </?= $this->translate("blog") ?></a>
                             </div>
                         </?php } else { ?> -->
@@ -50,10 +58,15 @@ if($_COOKIE["customer"] != "null") {
             <div class="container">
                 <div class="row">
                     <div class="col-md-3 col-sm-4 header-bottom-logo">
-                        <a href="<?php echo "/".$this->getLocale(); ?>">
-                            <img src="/static/images/logo-bfi.png" class="img-responsive" alt="">
-                        </a>
-
+                        <?php if ($site == "corporate") : ?>
+                            <a href="<?php echo "/".$this->getLocale()."/corporate"; ?>">
+                                <img src="/static/images/logo-bfi.png" class="img-responsive" alt="">
+                            </a>
+                        <?php else : ?>
+                            <a href="<?php echo "/".$this->getLocale(); ?>">
+                                <img src="/static/images/logo-bfi.png" class="img-responsive" alt="">
+                            </a>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
